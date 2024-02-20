9ae3b3f28810387bffb22d0ac1d6eeac8ad5209c
satiko900 <r.ueno.0706@gmail.com>
2024-02-20 20:59:49 +0900

    kadai_010を追加

---
+++ kadai_010/summary.js
@@ -0,0 +1,27 @@
+$(function () {
+
+  // id属性がchange-colorの要素がクリックされたら
+  $('#change-color').on('click', function () {
+    // 文字の色が変化する
+    $('#target').css('color', 'red');
+  });
+
+  // id属性がchange-textの要素がクリックされたら
+  $('#change-text').on('click', function () {
+    // テキストが「Hello!」に変化する
+    $('#target').text('Hello!');
+  });
+  
+  // id属性がfade-outの要素がクリックされたら
+  $('#fade-out').on('click', function () {
+    // フェードアウトする
+    $('#target').fadeOut();
+  });
+  
+  // id属性がfade-inの要素がクリックされたら
+  $('#fade-in').on('click', function () {
+    // フェードインする
+    $('#target').fadeIn();
+  });
+  
+});
